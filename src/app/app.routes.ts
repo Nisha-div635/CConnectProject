311d9e203b8f44c3a1c783e2d6b9670f345f1a39
Nisha Patra <nishakvian580@gmail.com>
2025-08-07 09:02:34 +0530

    Started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,51 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Login } from './login/login';
+import { Gallery } from './gallery/gallery';
+import { About } from './about/about';
+import { Events } from './events/events';
+import { Profile } from './profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component:Home,
+    },
+    {
+        path:'header',
+        component:Header,
+    },
+    {
+        path:'footer',
+        component:Footer,
+    },
+    {
+        path:'Login',
+        component:Login,
+    },
+    {
+        path:'gallery',
+        component: Gallery,
+    },
+    {
+        path:'about',
+        component:About,
+    },
+    {
+        path:'events',
+        component:Events,
+    },
+    {
+        path:'profile',
+        component:Profile,
+    },
+    
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink } from '@angular/router';
+import { Home } from './home/home';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+import { Login } from './login/login';
+import { Gallery } from './gallery/gallery';
+import { Events } from './events/events';
+import { About } from './about/about';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,Home,Login,Header,Footer,Profile,About,Events,Gallery],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
