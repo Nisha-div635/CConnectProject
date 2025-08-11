2bfdfb5792977c6410b195e85bdcbcf715d39811
Nisha Patra <nishakvian580@gmail.com>
2025-08-11 15:50:15 +0530

    started

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink,RouterOutlet,],
+  imports: [RouterLink,RouterOutlet,Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
